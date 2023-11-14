c243f456d6d87fd01c76e5017c993d53ed1fe977
BeerStrike <dimkomarof@gmail.com>
2023-11-14 12:04:50 +0300

    lnkn

--- Crsch_4/OpenGLENG/ShaderFuncs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <GL/glew.h> 
-#include <GLFW/glfw3.h> 
-
-namespace OpenGLENG {
-	GLuint LoadShader(std::string filename, GLenum type);
-}
--- Crsch_4/main.cpp
+++ Crsch_4/main.cpp
@@ -1,7 +1,5 @@
-#include "MainGameClass.h"
 
 int main() {
-    MainGameClass::startGame(640,480);
     return 0;
 }
 
